0914ed2d0f2d9eaec80951b505c66d6198fdbbc5
Filip <filip@filip.home>
2022-03-16 07:18:08 +0100

    fix data fetching

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
