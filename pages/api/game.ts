3f18d0d6c1632821ae8a52e3850a0fc9e5c95087
Filip <filip@filip.home>
2022-03-16 07:43:50 +0100

    fix

--- pages/api/game.ts
+++ pages/api/game.ts
@@ -1,12 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse){
- try {
-  const r = await fetch("http://35.224.173.125/");
-  const data = await r.json();
-  if(!data) return res.status(400)
-  res.status(200).json(data)
- } catch (error) {
-   res.status(400)
- }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const r = await fetch("http://35.224.173.125/");
+    const data = await r.json();
+    if (!data) return res.status(400);
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(400);
+  }
 }
